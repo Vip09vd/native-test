1acdc034667ffee0609d6e5aae71bbfc7aa74259
Vadim <vip09vd@gmail.com>
2018-01-18 15:51:33 +0200

    [FIX] comma

--- app/Services/store.service.js
+++ app/Services/store.service.js
@@ -3,20 +3,20 @@ class StoreService {
 		{
 			id: 1,
 			name: 'iPhone X',
-			image: 'http://zhzh.info/_pu/107/83911675.jpg'
+			image: 'http://zhzh.info/_pu/107/83911675.jpg',
 			price: 1000,
 		},
 		{
 			id: 2,
 			name: 'Galaxy S9',
-			image: 'https://cdn.images.express.co.uk/img/dynamic/galleries/x701/312541.jpg'
+			image: 'https://cdn.images.express.co.uk/img/dynamic/galleries/x701/312541.jpg',
 			price: 900,
 
 		},
 		{
 			id: 3,
 			name: 'Pixel 2',
-			image: 'https://blogs-images.forbes.com/gordonkelly/files/2017/07/untitled.5236.jpg'
+			image: 'https://blogs-images.forbes.com/gordonkelly/files/2017/07/untitled.5236.jpg',
 			price: 1100,
 		}
 	];
